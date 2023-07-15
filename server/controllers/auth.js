fdb4bd346bd1e843021a516762ad3e40b002f7f0
smoreyaks <bnixon.eng@gmail.com>
2023-07-15 23:07:23 +1000

    newUser Object & res 201 & 500

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -26,6 +26,21 @@ export const register = async (req, res) => {
         // Salt password
         // Make sure password is correct
         // Give JSON Web Token
-        const newUser = new User({});
-    } catch (err) {}
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
