2af4d85d397490c6a1762b2cb0eb3efedae58cda
smoreyaks <bnixon.eng@gmail.com>
2023-07-16 22:39:15 +1000

    Additional Comments

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,6 +38,8 @@ export const register = async (req, res) => {
             viewedProfile: Math.floor(Math.random() * 10000),
             impressions: Math.floor(Math.random() * 10000),
         });
+
+        // Understand Save() Function  & Res Status
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     } catch (err) {
