79b3c57239051ae0188670b66840b65f6eeab474
smoreyaks <bnixon.eng@gmail.com>
2023-08-20 23:57:44 +1000

    Added Try/Catch & Err Message to getUserFriends

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -11,22 +11,34 @@ export const getUser = async (req, res) => {
 };
 // Get User Friends API CAll
 export const getUserFriends = async (req, res) => {
-    const { id } = req.params;
-    const user = await User.findById(id);
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
 
-    const friends = await Promise.all(
-        user.friends.map((id) => User.findById(id))
-    );
-    const formattedFriends = friends.map(
-        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-            return {
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({
                 _id,
                 firstName,
                 lastName,
                 occupation,
                 location,
                 picturePath,
-            };
-        }
-    );
+            }) => {
+                return {
+                    _id,
+                    firstName,
+                    lastName,
+                    occupation,
+                    location,
+                    picturePath,
+                };
+            }
+        );
+        res.status(200).json(formattedFriends);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
 };
