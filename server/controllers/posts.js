225865138de0b49e82b5b13258434c53588ed842
smoreyaks <bnixon.eng@gmail.com>
2023-08-24 23:57:26 +1000

    Setup createPost Controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,32 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
 
-/* Complete Post Setup */
+/* Create */
+export const createPost = async (req, res) => {
+    try {
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: [],
+        });
+
+        // Save Post
+        await newPost.save();
+
+        // Grab the list of all the posts
+        const post = await Post.find();
+
+        // Return
+        res.status(201).json(post);
+    } catch (err) {
+        res.status(409).json({ message: err.message });
+    }
+};
