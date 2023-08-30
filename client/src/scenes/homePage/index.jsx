c9fe6651f490780b90b71106f338e836e8cf6606
smoreyaks <bnixon.eng@gmail.com>
2023-08-30 22:20:45 +1000

    Import Navbar Component

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,18 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "../navbar";
+// import UserWidget from "scenes/widgets/UserWidget";
+// import MyPostWidget from "scenes/widgets/MyPostWidget";
+// import PostsWidget from "scenes/widgets/PostsWidget";
+// import AdvertWidget from "scenes/widgets/AdvertWidget";
+// import FriendListWidget from "scenes/widgets/FriendListWidget";
+
 const HomePage = () => {
-    return <div>Homepage</div>;
+    return (
+        <Box>
+            <Navbar />
+        </Box>
+    );
 };
 
 export default HomePage;
