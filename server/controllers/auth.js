736e9d28544f8760060beed7f7ef9835abe949ac
smoreyaks <bnixon.eng@gmail.com>
2023-08-31 15:31:57 +1000

    Added Response for Login to Send JSON to Frontend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -56,18 +56,19 @@ export const login = async (req, res) => {
         const user = await User.findOne({ email: email });
 
         // User Error Handling
-        if (!user) {
+        if (!user)
             return res.status(400).json({ msg: "User does not exist. " });
-        }
+
         // Validates user Email & Password Credentials
         const isMatch = await bcrypt.compare(password, user.password);
+
         // Password Error Handling
-        if (!isMatch) {
+        if (!isMatch)
             return res.status(400).json({ msg: "Invalid credentials. " });
-        }
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         delete user.password;
+        res.status(200).json({ token, user });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
