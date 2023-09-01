91178330cc5393c8e5ddfe30dc10f7d0f8ec5010
smoreyaks <bnixon.eng@gmail.com>
2023-09-02 09:11:04 +1000

    Setup Imports, State, HandlePost & Status Bar

---
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,90 @@
+// MUI Components
+import {
+    Box,
+    Divider,
+    Typography,
+    InputBase,
+    useTheme,
+    Button,
+    IconButton,
+    useMediaQuery,
+} from "@mui/material";
+
+// MUI Icons
+import {
+    EditOutlined,
+    DeleteOutlined,
+    AttachFileOutlined,
+    GidBoxOutlined,
+    ImageOutlined,
+    MicOutlined,
+    MoreHorizOutlined,
+} from "@mui/icons-material";
+
+import FlexBetween from "../../components/FlexBetween";
+import Dropzone from "react-dropzone";
+import UserImage from "../../components/UserImage";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state";
+
+const MyPostWidget = ({ picturePath }) => {
+    // State
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(false);
+    const [post, setPost] = useState("");
+
+    // Theme
+    const { palette } = useTheme();
+
+    // Redux State
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.mneutral.medium;
+
+    const handlePost = async () => {
+        const formData = new FormData();
+        formData.append("userId", _id);
+        formData.append("description", post);
+        if (image) {
+            formData.append("picture", image);
+            formData.append("picturePath", image.name);
+        }
+
+        const response = await fetch(`http://localhost:3001/posts`, {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData,
+        });
+        const posts = await response.json();
+        dispatch(setPosts({ posts }));
+        setImage(null);
+        setPost("");
+    };
+
+    return (
+        <WidgetWrapper>
+            <FlexBetween gap="1.5rem">
+                <UserImage image={picturePath} />
+                <InputBase
+                    placeholder="What's on your mind?"
+                    onChange={(e) => setPost(e.target.value)}
+                    value={post}
+                    sx={{
+                        width: "100%",
+                        backgroundColor: palette.neutral.light,
+                        borderRadius: "2rem",
+                        padding: "1rem 2rem",
+                    }}
+                />
+            </FlexBetween>
+            {isImage && <Box></Box>}
+        </WidgetWrapper>
+    );
+};
+
+export default MyPostWidget;
