0b6635e76c115ed424e648840ebd4418256830f3
smoreyaks <bnixon.eng@gmail.com>
2023-09-01 14:53:41 +1000

    Saved Filtered Friends List to user.friends

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,6 +5,7 @@ export const getUser = async (req, res) => {
     try {
         const { id } = req.params;
         const user = await User.findById(id);
+        res.status(200).json(user);
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
@@ -51,7 +52,7 @@ export const addRemoveFriend = async (req, res) => {
         const friend = await User.findById(friendId);
 
         if (user.friends.includes(friendId)) {
-            user.friends.filter((id) => id !== friendId);
+            user.friends = user.friends.filter((id) => id !== friendId);
             friend.friends = friend.friends.filter((id) => id !== id);
         } else {
             user.friends.push(friendId);
@@ -82,6 +83,7 @@ export const addRemoveFriend = async (req, res) => {
                 };
             }
         );
+
         res.status(200).json(formattedFriends);
     } catch (err) {
         res.status(404).json({ message: err.message });
