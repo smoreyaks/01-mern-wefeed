8dfb6507082e7895956bf9b9126aa46c0b3e47de
smoreyaks <bnixon.eng@gmail.com>
2023-09-05 01:35:50 +1000

    Liked & Comments Section Layout & Styles

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -4,14 +4,12 @@ import {
     FavoriteOutlined,
     ShareOutlined,
 } from "@mui/icons-material";
-
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/matieral";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import Friend from "../../components/Friend";
 import WidgetWrapper from "../../components/WidgetWrapper";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { setPost } from "../../state";
 
 const PostWidget = ({
@@ -27,16 +25,14 @@ const PostWidget = ({
 }) => {
     const [isComments, setIsComments] = useState(false);
     const dispatch = useDispatch();
-    const navigate = useNavigate();
     const token = useSelector((state) => state.token);
     const loggedInUserId = useSelector((state) => state.user._id);
-    const isLiked = Boolean(likes([loggedInUserId]));
-
+    const isLiked = Boolean(likes[loggedInUserId]);
     const likeCount = Object.keys(likes).length;
 
     const { palette } = useTheme();
     const main = palette.neutral.main;
-    const medium = palette.primary.main;
+    const primary = palette.primary.main;
 
     const patchLike = async () => {
         const response = await fetch(
@@ -61,24 +57,61 @@ const PostWidget = ({
                 name={name}
                 subtitle={location}
                 userPicturePath={userPicturePath}
-            >
-                {" "}
-                <Typography color={main} sx={{ mt: "1rem" }}>
-                    {description}
-                </Typography>
-                {picturePath && (
-                    <img
-                        width="100%"
-                        height="auto"
-                        alt="post"
-                        style={{
-                            borderRadius: "0.75rem",
-                            marginTop: "0.75rem",
-                        }}
-                        src={`http://localhost:3001/assets/${picturePath}`}
-                    />
-                )}
-            </Friend>
+            />
+            <Typography color={main} sx={{ mt: "1rem" }}>
+                {description}
+            </Typography>
+            {picturePath && (
+                <img
+                    width="100%"
+                    height="auto"
+                    alt="post"
+                    style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+                    src={`http://localhost:3001/assets/${picturePath}`}
+                />
+            )}
+            <FlexBetween mt="0.25rem">
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={patchLike}>
+                            {isLiked ? (
+                                <FavoriteOutlined sx={{ color: primary }} />
+                            ) : (
+                                <FavoriteBorderOutlined />
+                            )}
+                        </IconButton>
+                        <Typography>{likeCount}</Typography>
+                    </FlexBetween>
+
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={() => setIsComments(!isComments)}>
+                            <ChatBubbleOutlineOutlined />
+                        </IconButton>
+                        <Typography>{comments.length}</Typography>
+                    </FlexBetween>
+                </FlexBetween>
+
+                <IconButton>
+                    <ShareOutlined />
+                </IconButton>
+            </FlexBetween>
+            {isComments && (
+                <Box mt="0.5rem">
+                    {comments.map((comment, i) => (
+                        <Box key={`${name}-${i}`}>
+                            <Divider />
+                            <Typography
+                                sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}
+                            >
+                                {comment}
+                            </Typography>
+                        </Box>
+                    ))}
+                    <Divider />
+                </Box>
+            )}
         </WidgetWrapper>
     );
 };
+
+export default PostWidget;
