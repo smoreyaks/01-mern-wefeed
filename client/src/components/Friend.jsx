cf02440a357e6f3d5349008fbbf9c9d4eab85710
smoreyaks <bnixon.eng@gmail.com>
2023-09-05 20:24:46 +1000

    Fixed friends destructuring & find function

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -1,5 +1,4 @@
 import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
-
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -12,7 +11,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const navigate = useNavigate();
     const { _id } = useSelector((state) => state.user);
     const token = useSelector((state) => state.token);
-    const { friends } = useSelector((state) => state.friends);
+    const friends = useSelector((state) => state.user.friends);
 
     const { palette } = useTheme();
     const primaryLight = palette.primary.light;
@@ -20,7 +19,8 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const main = palette.neutral.main;
     const medium = palette.neutral.medium;
 
-    const isFriend = friends.find((friend) => friend._id) === friendId;
+    const isFriend = friends.find((friend) => friend._id === friendId);
+
     const patchFriend = async () => {
         const response = await fetch(
             `http://localhost:3001/users/${_id}/${friendId}`,
