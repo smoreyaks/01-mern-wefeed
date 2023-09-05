e4a3cfa5bfa1165f0e05a91f686a1af41fbf068a
smoreyaks <bnixon.eng@gmail.com>
2023-09-05 12:17:03 +1000

    Fixed SetPosts and SetPost

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -26,12 +26,15 @@ export const authSlice = createSlice({
             if (state.user) {
                 state.user.friends = action.payload.friends;
             } else {
-                console.error("User friends does not exist");
+                console.error("user friends non-existent :(");
             }
         },
         setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
             const updatedPosts = state.posts.map((post) => {
-                if (post._id === action.payload.post_id)
+                if (post._id === action.payload.post._id)
                     return action.payload.post;
                 return post;
             });
