8c615f9ae3c77386a520ee9426ff7395f17c632d
smoreyaks <bnixon.eng@gmail.com>
2023-09-06 17:43:39 +1000

    Fixed Friends Express Route Access Path

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,11 +8,11 @@ import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-/* Read */
+/* READ */
 router.get("/:id", verifyToken, getUser);
-router.get("./:friends", verifyToken, getUserFriends);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
-/* Update */
+/* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
