0e3fe2ae8a5d77a1f55db66cb283d11a45ebc552
smoreyaks <bnixon.eng@gmail.com>
2023-09-29 17:42:45 +1000

    Abstracted Away Port Values into env.local

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,5 +1,10 @@
 import { Box } from "@mui/material";
 
+// Environment Variable
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 6001;
 const UserImage = ({ image, size = "60px" }) => {
     return (
         <Box width={size} height={size}>
@@ -11,7 +16,7 @@ const UserImage = ({ image, size = "60px" }) => {
                 width={size}
                 height={size}
                 alt="user"
-                src={`http://localhost:3005/assets/${image}`}
+                src={`http://localhost:${PORT}/assets/${image}`}
             />
         </Box>
     );
