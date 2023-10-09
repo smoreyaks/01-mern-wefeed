e05b4178a16c62b9ab634dec701d421a12e3d9be
smoreyaks <bnixon.eng@gmail.com>
2023-10-09 16:36:00 +1100

    Created Background Component

---
+++ client/src/components/Background.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const Background = styled("Box", {
+    shouldForwardProp: (prop) => prop !== "backgroundThemeImg",
+    name: "Background",
+    slot: "Root",
+})(({ backgroundThemeImg }) => ({
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${backgroundThemeImg})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+}));
+
+export default Background;
