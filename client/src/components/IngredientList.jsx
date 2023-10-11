8b35d7cc5e7e8d08fe7fc305bcd1777c2fd7a02c
smoreyaks <bnixon.eng@gmail.com>
2023-10-12 02:37:08 +1100

    Added Expand & Collapse Functionality

--- client/src/components/IngredientList.jsx
+++ client/src/components/IngredientList.jsx
@@ -1,6 +1,19 @@
+// React
+import { useState } from "react";
+
+// MUI Components
 import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
 
+// MUI Icons
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+// Components
+import FlexBetween from "./FlexBetween";
+
 const IngredientList = ({ ingredients }) => {
+    const [ingredientListOpen, setIngredientListOpen] = useState(false);
+
     // Theme
     const { palette } = useTheme();
     const main = palette.default.neutral.main;
@@ -9,20 +22,32 @@ const IngredientList = ({ ingredients }) => {
     return (
         <Box
             sx={{
-                p: "0 0.5rem",
-                mt: "0.5rem",
+                p: "0.25rem 0.5rem",
+                m: "0.5rem 0",
                 backgroundColor: main,
+                borderRadius: "0.75rem",
             }}
         >
-            <Typography p=".5rem 0">
-                Ingredients
-                {ingredients.map((ingredient) => (
+            <FlexBetween>
+                <Typography p=".5rem 0" variant="h5" borderRadius="0.75rem">
+                    Ingredients
+                </Typography>
+                <IconButton
+                    onClick={() => setIngredientListOpen(!ingredientListOpen)}
+                >
+                    {ingredientListOpen ? <RemoveIcon /> : <AddIcon />}
+                </IconButton>
+            </FlexBetween>
+            {ingredients.map((ingredient) =>
+                ingredientListOpen ? (
                     <Box
                         mr="0.5rem"
                         sx={{
                             display: "grid",
                             gridTemplateColumns: "repeat(2, 2fr)",
                             gap: "0.5rem",
+
+                            borderRadius: "0.75rem",
                         }}
                     >
                         <Box gridColumn="1">
@@ -43,8 +68,10 @@ const IngredientList = ({ ingredients }) => {
                             </Typography>
                         </Box>
                     </Box>
-                ))}
-            </Typography>
+                ) : (
+                    <Box display="none" />
+                )
+            )}
         </Box>
     );
 };
