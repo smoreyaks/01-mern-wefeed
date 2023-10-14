208fd8d898b23590398d827ac199e07a07983428
smoreyaks <bnixon.eng@gmail.com>
2023-10-14 23:56:12 +1100

    Created StepList Component

---
+++ client/src/components/Steps.jsx
@@ -0,0 +1,21 @@
+// React
+import { useState } from "react";
+
+// MUI Components
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "./FlexBetween";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+
+// State
+import { setRecipe } from "../../state";
+
+// Components
+import FlexBetween from "../../components/FlexBetween";
+
+const StepList = () => {
+    return <FlexBetween></FlexBetween>;
+};
+
+export default StepList;
