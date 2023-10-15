adcec9f77e3ce3842fbd2ba95dab5b4d5f053570
smoreyaks <bnixon.eng@gmail.com>
2023-10-16 02:46:09 +1100

    Recreated StepsList Component

---
+++ client/src/components/StepsList.jsx
@@ -0,0 +1,12 @@
+// React
+import { useState } from "react";
+
+// MUI Components
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "./FlexBetween";
+
+const StepsList = () => {
+    return <Box></Box>;
+};
+
+export default StepsList;
