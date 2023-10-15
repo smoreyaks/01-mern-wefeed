179079f6f212f387382823bcdc0c5c4e45cb145a
smoreyaks <bnixon.eng@gmail.com>
2023-10-16 02:56:19 +1100

    StepsList Layout, Styling & List Mapping - SAFE

--- client/src/components/StepsList.jsx
+++ client/src/components/StepsList.jsx
@@ -2,11 +2,74 @@
 import { useState } from "react";
 
 // MUI Components
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+
+// MUI Icons
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+// Components
 import FlexBetween from "./FlexBetween";
 
 const StepsList = () => {
-    return <Box></Box>;
+    const [stepsListOpen, setStepsListOpen] = useState(false);
+
+    // Theme
+    const { palette } = useTheme();
+    const main = palette.default.neutral.main;
+    const primary = palette.default.primary.main;
+
+    return (
+        <Box
+            sx={{
+                p: "0.25rem 0.5rem",
+                m: "0.5rem 0",
+                backgroundColor: main,
+                borderRadius: "0.75rem",
+            }}
+        >
+            <FlexBetween>
+                <Typography p=".5rem 0" variant="h5" borderRadius="0.75rem">
+                    Steps
+                </Typography>
+                <IconButton onClick={() => setStepsListOpen(!stepsListOpen)}>
+                    {stepsListOpen ? <RemoveIcon /> : <AddIcon />}
+                </IconButton>
+            </FlexBetween>
+            {steps.map((step) =>
+                stepsListOpen ? (
+                    <Box
+                        mr="0.5rem"
+                        sx={{
+                            display: "grid",
+                            gridTemplateColumns: "repeat(2, 2fr)",
+                            gap: "0.5rem",
+                            borderRadius: "0.75rem",
+                        }}
+                    >
+                        <Box gridColumn="1">
+                            <Typography
+                                key={step.stepNum}
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "flex-start",
+                                }}
+                            >
+                                {step.stepNum}
+                            </Typography>
+                        </Box>
+                        <Box gridColumn="2">
+                            <Typography key={step.stepNum}>
+                                {step.stepNum}
+                            </Typography>
+                        </Box>
+                    </Box>
+                ) : (
+                    <Box display="none" />
+                )
+            )}
+        </Box>
+    );
 };
 
 export default StepsList;
