32d2ed66224c9988add2a8c6a84fb36fb5370869
smoreyaks <bnixon.eng@gmail.com>
2023-10-16 02:44:06 +1100

    Removed PostWidget Import

--- client/src/scenes/widgets/RecipesFeedWidget.jsx
+++ client/src/scenes/widgets/RecipesFeedWidget.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setRecipes } from "../../state";
-import PostWidget from "./PostWidget";
 
 // RecipePost
 import RecipePostWidget from "./RecipePostWidget";
