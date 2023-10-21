795b7f1c53e9cb614a92f79dd70b8700cb13b0d3
smoreyaks <bnixon.eng@gmail.com>
2023-10-21 17:59:26 +1100

    Added Background Color & Image to Body

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 // Import Packages
-
 import { useState, useEffect } from "react";
 
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
@@ -8,7 +7,14 @@ import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import {
+    Box,
+    CardMedia,
+    CssBaseline,
+    GlobalStyles,
+    ScopedCssBaseline,
+    ThemeProvider,
+} from "@mui/material";
 
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
@@ -20,6 +26,9 @@ function App() {
     const isAuth = Boolean(useSelector((state) => state.token));
     const token = useSelector((state) => state.token);
     const { _id, picturePath } = useSelector((state) => state.user || {});
+
+    // Theme
+    const backgroundThemeColor = theme.palette.default.background.default;
 
     // State
     const [user, setUser] = useState(null);
@@ -36,7 +45,6 @@ function App() {
         setUser(data);
         console.log("UserWidget.js - DATA - ", data);
     };
-
     useEffect(() => {
         getUser();
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
@@ -59,28 +67,51 @@ function App() {
     console.log("THEME_IMG:", backgroundThemeImg);
 
     return (
-        <Background backgroundThemeImg={backgroundThemeImg}>
+        <CardMedia
+            src={`https://server-vukx.onrender.com/assets/dessertTheme/${backgroundThemeImg}`}
+        >
             <BrowserRouter>
                 <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Routes>
-                        <Route path="/" element={<LoginPage />} />
-                        <Route
-                            path="/home"
-                            element={
-                                isAuth ? <HomePage /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route
-                            path="/profile/:userId"
-                            element={
-                                isAuth ? <ProfilePage /> : <Navigate to="/" />
-                            }
-                        />
-                    </Routes>
+                    <GlobalStyles
+                        styles={{
+                            body: {
+                                backgroundColor: backgroundThemeColor,
+                                backgroundImage: `url(https://server-vukx.onrender.com/assets/dessertTheme/${backgroundThemeImg})`,
+                                margin: 0,
+                                padding: 0,
+                            },
+                        }}
+                    />
+                    <ScopedCssBaseline
+                        enableColorScheme
+                        sx={{
+                            backgroundColor: backgroundThemeColor,
+                            backgroundImage: `url(https://server-vukx.onrender.com/assets/dessertTheme/${backgroundThemeImg})`,
+                        }}
+                    >
+                        <Routes>
+                            <Route path="/" element={<LoginPage />} />
+                            <Route
+                                path="/home"
+                                element={
+                                    isAuth ? <HomePage /> : <Navigate to="/" />
+                                }
+                            />
+                            <Route
+                                path="/profile/:userId"
+                                element={
+                                    isAuth ? (
+                                        <ProfilePage />
+                                    ) : (
+                                        <Navigate to="/" />
+                                    )
+                                }
+                            />
+                        </Routes>
+                    </ScopedCssBaseline>
                 </ThemeProvider>
             </BrowserRouter>
-        </Background>
+        </CardMedia>
     );
 }
 
