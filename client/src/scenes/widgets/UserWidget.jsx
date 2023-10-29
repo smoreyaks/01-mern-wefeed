f2eee3a654e66093e4e56c9514be2e1e5c48f490
smoreyaks <bnixon.eng@gmail.com>
2023-10-30 04:12:49 +1100

    White Text & themeColors Prop

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -12,7 +12,7 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const UserWidget = ({ userId, picturePath }) => {
+const UserWidget = ({ userId, picturePath, themeColors }) => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
     const token = useSelector((state) => state.token);
@@ -22,6 +22,19 @@ const UserWidget = ({ userId, picturePath }) => {
     const dark = palette.default.neutral.dark;
     const medium = palette.default.neutral.medium;
     const main = palette.default.neutral.main;
+
+    const {
+        headingText,
+        recipeText,
+        recipeStepsPanel,
+        textHover,
+        whiteText,
+        followerIconOutline,
+        followerIconBack,
+        followerIconBackHover,
+        recipeTextPanel,
+        mainBackPanel,
+    } = themeColors;
 
     const getUser = async () => {
         const response = await fetch(
@@ -68,18 +81,18 @@ const UserWidget = ({ userId, picturePath }) => {
                     <Box>
                         <Typography
                             variant="h4"
-                            color={dark}
+                            color={whiteText}
                             fontWeight="500"
                             sx={{
                                 "&:hover": {
-                                    color: palette.default.primary.light,
+                                    color: whiteText,
                                     cursor: "pointer",
                                 },
                             }}
                         >
                             {firstName} {lastName}
                         </Typography>
-                        <Typography color={medium}>
+                        <Typography color={whiteText}>
                             {friends.length} friends
                         </Typography>
                     </Box>
@@ -92,15 +105,18 @@ const UserWidget = ({ userId, picturePath }) => {
             {/* Second Row */}
             <Box p="1rem 0">
                 <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
-                    <LocationOnOutlined fontSize="large" sx={{ color: main }} />
-                    <Typography color={medium}>{location}</Typography>
+                    <LocationOnOutlined
+                        fontSize="large"
+                        sx={{ color: whiteText }}
+                    />
+                    <Typography color={whiteText}>{location}</Typography>
                 </Box>
                 <Box display="flex" alignItems="center" gap="1rem">
                     <WorkOutlineOutlined
                         fontSize="large"
-                        sx={{ color: main }}
+                        sx={{ color: whiteText }}
                     />
-                    <Typography color={medium}>{occupation}</Typography>
+                    <Typography color={whiteText}>{occupation}</Typography>
                 </Box>
             </Box>
 
@@ -109,18 +125,18 @@ const UserWidget = ({ userId, picturePath }) => {
             {/* Third Row */}
             <Box p="1rem 0">
                 <FlexBetween mb="0.5rem">
-                    <Typography color={medium}>
+                    <Typography color={whiteText}>
                         Who's viewed your profile
                     </Typography>
-                    <Typography color={main} fontWeight="500">
+                    <Typography color={whiteText} fontWeight="500">
                         {viewedProfile}
                     </Typography>
                 </FlexBetween>
                 <FlexBetween>
-                    <Typography color={medium}>
+                    <Typography color={whiteText}>
                         Impressions of your recipe
                     </Typography>
-                    <Typography color={main} fontWeight="500">
+                    <Typography color={whiteText} fontWeight="500">
                         {impressions}
                     </Typography>
                 </FlexBetween>
@@ -132,7 +148,7 @@ const UserWidget = ({ userId, picturePath }) => {
             <Box p="1rem 0">
                 <Typography
                     fontSize="1rem"
-                    color={main}
+                    color={whiteText}
                     fontWeight="500"
                     mb="1rem"
                 >
@@ -143,25 +159,25 @@ const UserWidget = ({ userId, picturePath }) => {
                     <FlexBetween gap="1rem">
                         <img src="../assets/twitter.png" alt="twitter" />
                         <Box>
-                            <Typography color={main} fontWeight="500">
+                            <Typography color={whiteText} fontWeight="500">
                                 Twitter
                             </Typography>
-                            <Typography color={medium}>
+                            <Typography color={whiteText}>
                                 Social Network
                             </Typography>
                         </Box>
                     </FlexBetween>
-                    <EditOutlined sx={{ color: main }} />
+                    <EditOutlined sx={{ color: whiteText }} />
                 </FlexBetween>
 
                 <FlexBetween gap="1rem">
                     <FlexBetween gap="1rem">
                         <img src="../assets/linkedin.png" alt="linkedin" />
                         <Box>
-                            <Typography color={main} fontWeight="500">
+                            <Typography color={whiteText} fontWeight="500">
                                 LinkedIn
                             </Typography>
-                            <Typography color={medium}>
+                            <Typography color={whiteText}>
                                 Network Platform
                             </Typography>
                         </Box>
