dd9195c423ee214c7c261f5d6640960b185c5d5c
smoreyaks <bnixon.eng@gmail.com>
2023-11-03 14:34:21 +1100

    Updated Text Variable

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -1,20 +1,33 @@
-import { Typography, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
 
-const AdvertWidget = () => {
+const AdvertWidget = ({ themeColors }) => {
     const { palette } = useTheme();
     const dark = palette.default.neutral.dark;
     const main = palette.default.neutral.main;
     const medium = palette.default.neutral.medium;
 
+    const {
+        headingText,
+        recipeText,
+        recipeStepsPanel,
+        textHover,
+        whiteText,
+        followerIconOutline,
+        followerIconBack,
+        followerIconBackHover,
+        recipeTextPanel,
+        mainBackPanel,
+    } = themeColors || {};
+
     return (
         <WidgetWrapper>
             <FlexBetween>
-                <Typography color={dark} variant="h5" fontWeight="500">
+                <Typography color={headingText} variant="h5" fontWeight="500">
                     Sponsored
                 </Typography>
-                <Typography color={medium}>Create Ad</Typography>
+                <Typography color={headingText}>Create Ad</Typography>
             </FlexBetween>
             <img
                 width="100%"
@@ -23,11 +36,10 @@ const AdvertWidget = () => {
                 src="https://server-vukx.onrender.com/assets/info4.jpeg"
                 style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
             />
-            <FlexBetween>
-                <Typography color={main}>MikaCosmetics</Typography>
-                <Typography color={medium}>MikaCosmetics.com</Typography>
-            </FlexBetween>
-            <Typography color={medium} m="0.5rem 0">
+
+            <Typography color={headingText}>MikaCosmetics.com</Typography>
+
+            <Typography color={headingText} m="0.5rem 0">
                 Your pathway to stunning, immaculate beauty & made sure to make
                 your skin is shining like light.
             </Typography>
