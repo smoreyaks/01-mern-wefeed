f668655905899e84a5671b2ddb8710f806bbaf13
smoreyaks <bnixon.eng@gmail.com>
2023-12-18 13:57:23 +1100

    Modified Filter List - Filter Functionality TBC

---
+++ client/src/components/ProjectFilter.js
@@ -0,0 +1,40 @@
+// React
+import React from "react";
+
+// Hooks
+import { useState } from "react";
+
+const filterList = [
+    ["All", "All"],
+    ["main", "Main"],
+    ["appetiser", "Appetiser"],
+    ["breakfast", "Breakfast"],
+    ["dessert", "Dessert"],
+    ["drink", "Drink"],
+];
+
+export default function ProjectFilter({ changeFilter }) {
+    const [currentFilter, setCurrentFilter] = useState("all");
+
+    const handleClick = (newFilter) => {
+        setCurrentFilter(newFilter);
+        changeFilter(newFilter);
+    };
+
+    return (
+        <div className="project-filter">
+            <nav>
+                <p>Filter By:</p>
+                {filterList.map((f) => (
+                    <button
+                        key={f}
+                        onClick={() => handleClick(f[0])}
+                        className={currentFilter === f[0] ? "active" : ""}
+                    >
+                        {f[1]}
+                    </button>
+                ))}
+            </nav>
+        </div>
+    );
+}
