896b5107d7431ad4e9fa7c07755385b6f2bf3d2c
smoreyaks <bnixon.eng@gmail.com>
2023-12-22 02:35:23 +1100

    Setup Component for Initial Testing

--- client/src/components/FeedSelect.jsx
+++ client/src/components/FeedSelect.jsx
@@ -1,62 +1,92 @@
 import { useState } from "react";
-import { useAuthContext } from "../../hooks/useAuthContext";
+// import { useAuthContext } from "../../hooks/useAuthContext";
 
 // Styles
 // import "./Dashboard.css";
 
 // Components
-import ProjectList from "../../components/ProjectList";
-import ProjectFilter from "./ProjectFilter";
+// import ProjectList from "../../components/ProjectList";
+// import ProjectFilter from "./ProjectFilter";
 
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
+// Components
+import FlexBetween from "./FlexBetween";
+import WidgetWrapper from "./WidgetWrapper";
+import { IconButton } from "@mui/material";
+
+// Icons
+import LocalBarIcon from "@mui/icons-material/LocalBar";
+
+// Custom Icons
+import AppetiserIcon from "./svg/AppetiserIcon";
 
 export default function FeedSelect() {
-    const { user } = useAuthContext();
-    const { documents, error } = useCollection("projects");
+    // const { user } = useAuthContext();
+    // const { documents, error } = useCollection("projects");
     const [filter, setFilter] = useState("all");
 
     const changeFilter = (newFilter) => {
         setFilter(newFilter);
     };
 
-    const projects = documents
-        ? documents.filter((document) => {
-              switch (filter) {
-                  case "All":
-                      return true;
-                  case "main":
-                      let assignedToMe = false;
-                      console.log(document.assignedUsersList);
-                      document.assignedUsersList.forEach((u) => {
-                          if (u.id === user.uid) {
-                              assignedToMe = true;
-                          }
-                      });
-                      return assignedToMe;
-                  case "appetiser":
-                      let createdByMe = false;
-                      console.log(document.createdBy.id);
-                      if (document.createdBy.id === user.uid) {
-                          createdByMe = true;
-                      }
-                      return createdByMe;
-                  case "breakfast":
-                  case "dessert":
-                  case "drink":
-                      console.log(document.category, filter);
-                      return document.category === filter;
-                  default:
-                      return true;
-              }
-          })
-        : null;
+    // const projects = documents
+    //     ? documents.filter((document) => {
+    //           switch (filter) {
+    //               case "all":
+    //                   return true;
+    //               case "main":
+    //                   let assignedToMe = false;
+    //                   console.log(document.assignedUsersList);
+    //                   document.assignedUsersList.forEach((u) => {
+    //                       if (u.id === user.uid) {
+    //                           assignedToMe = true;
+    //                       }
+    //                   });
+    //                   return assignedToMe;
+    //               case "appetiser":
+    //                   let createdByMe = false;
+    //                   console.log(document.createdBy.id);
+    //                   if (document.createdBy.id === user.uid) {
+    //                       createdByMe = true;
+    //                   }
+    //                   return createdByMe;
+    //               case "breakfast":
+    //               case "dessert":
+    //               case "drink":
+    //                   console.log(document.category, filter);
+    //                   return document.category === filter;
+    //               default:
+    //                   return true;
+    //           }
+    //       })
+    //     : null;
 
     return (
-        <div>
-            <h2 className="page-title">Dashboard</h2>
-            {error && <p className="error">{error}</p>}
-            {documents && <ProjectFilter changeFilter={changeFilter} />}
-            {projects && <ProjectList projects={projects} />}
-        </div>
+        <WidgetWrapper>
+            <FlexBetween>
+                {/* All Recipes */}
+                <AppetiserIcon>All</AppetiserIcon>
+
+                {/* Main Dish Recipes */}
+                <IconButton>Main</IconButton>
+
+                {/* Appetiser Recipes */}
+                <IconButton>Appetisers</IconButton>
+
+                {/* Breakfast Recipes */}
+                <IconButton>Breakfast</IconButton>
+
+                {/* Dessert Recipes */}
+                <IconButton>Dessert</IconButton>
+
+                {/* Drinks */}
+                <IconButton>
+                    <LocalBarIcon />
+                </IconButton>
+            </FlexBetween>
+            {/* {error && <p className="error">{error}</p>} */}
+            {/* {documents && <ProjectFilter changeFilter={changeFilter} />} */}
+            {/* {projects && <ProjectList projects={projects} />} */}
+        </WidgetWrapper>
     );
 }
