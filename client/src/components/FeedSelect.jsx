b2df595f8c316fd50031bc5d46b0283506643cd1
smoreyaks <bnixon.eng@gmail.com>
2024-01-04 17:35:20 +1100

    Fixed Feed Select Button Styling

--- client/src/components/FeedSelect.jsx
+++ client/src/components/FeedSelect.jsx
@@ -21,15 +21,16 @@ import DessertIcon from "./svg/DessertIcon";
 import DrinkIcon from "./svg/DrinkIcon";
 import BreakfastIcon from "./svg/BreakfastIcon";
 
-import { Button, Typography } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 
 // Check button style
-export default function FeedSelect(themeColors) {
+const FeedSelect = ({ themeColors }) => {
+    const { palette } = useTheme();
     // const { user } = useAuthContext();
     // const { documents, error } = useCollection("projects");
     const [filter, setFilter] = useState("all");
 
-    const { textHover, headingText } = themeColors || {};
+    const { textHover, headingText, recipeStepsPanel } = themeColors || {};
 
     const changeFilter = (newFilter) => {
         setFilter(newFilter);
@@ -71,56 +72,139 @@ export default function FeedSelect(themeColors) {
         // Adjust Feed Select design
         <WidgetWrapper>
             <FlexBetween>
-                {/* All Recipes */}
-                <Button
-                    size="medium"
-                    fullwidth
-                    // onClick={patchLike}
+                <Box
                     sx={{
+                        display: "flex",
+                        backgroundColor: recipeStepsPanel,
                         borderRadius: "3rem",
-                        width: "6rem",
-
-                        color: headingText,
-                        "&:hover": {
-                            color: headingText,
-                            backgroundColor: textHover,
-                        },
                     }}
                 >
-                    <Typography>All</Typography>
-                </Button>
+                    {/* All Recipes */}
+                    <Button
+                        // size="medium"
+                        // fullwidth
+                        // // onClick={patchLike}
+                        // sx={{
+                        //     borderRadius: "3rem",
+                        //     width: "6rem",
 
-                {/* Main Dish Recipes */}
+                        //     color: headingText,
+                        //     "&:hover": {
+                        //         color: headingText,
+                        //         backgroundColor: textHover,
+                        //     },
+                        // }}
 
-                <Button>
-                    <MainIcon />
-                </Button>
+                        sx={{
+                            backgroundColor: recipeStepsPanel,
+                            color: headingText,
+                            p: "0.5rem",
+                            m: "0.5rem",
 
-                {/* Appetiser Recipes */}
+                            borderRadius: "3rem",
+                            "&:hover": {
+                                color: headingText,
+                                backgroundColor: textHover,
+                            },
+                        }}
+                    >
+                        <Typography>All</Typography>
+                    </Button>
 
-                <Button>
-                    <AppetiserIcon />
-                </Button>
+                    {/* Main Dish Recipes */}
 
-                {/* Breakfast Recipes */}
+                    <Button
+                        sx={{
+                            backgroundColor: recipeStepsPanel,
+                            color: headingText,
+                            p: "0.5rem",
+                            m: "0.5rem",
 
-                <Button>
-                    <BreakfastIcon />
-                </Button>
+                            borderRadius: "3rem",
+                            "&:hover": {
+                                backgroundColor: textHover,
+                            },
+                        }}
+                    >
+                        <MainIcon />
+                    </Button>
 
-                {/* Dessert Recipes */}
-                <Button>
-                    <DessertIcon />
-                </Button>
+                    {/* Appetiser Recipes */}
 
-                {/* Drinks */}
-                <Button>
-                    <DrinkIcon />
-                </Button>
+                    <Button
+                        sx={{
+                            backgroundColor: recipeStepsPanel,
+                            color: headingText,
+                            p: "0.5rem",
+                            m: "0.5rem",
+
+                            borderRadius: "3rem",
+                            "&:hover": {
+                                backgroundColor: textHover,
+                            },
+                        }}
+                    >
+                        <AppetiserIcon />
+                    </Button>
+
+                    {/* Breakfast Recipes */}
+
+                    <Button
+                        sx={{
+                            backgroundColor: recipeStepsPanel,
+                            color: headingText,
+                            p: "0.5rem",
+                            m: "0.5rem",
+
+                            borderRadius: "3rem",
+                            "&:hover": {
+                                backgroundColor: textHover,
+                            },
+                        }}
+                    >
+                        <BreakfastIcon />
+                    </Button>
+
+                    {/* Dessert Recipes */}
+                    <Button
+                        sx={{
+                            backgroundColor: recipeStepsPanel,
+                            color: headingText,
+                            p: "0.5rem",
+                            m: "0.5rem",
+
+                            borderRadius: "3rem",
+                            "&:hover": {
+                                backgroundColor: textHover,
+                            },
+                        }}
+                    >
+                        <DessertIcon />
+                    </Button>
+
+                    {/* Drinks */}
+                    <Button
+                        sx={{
+                            backgroundColor: recipeStepsPanel,
+                            color: headingText,
+                            p: "0.5rem",
+                            m: "0.5rem",
+
+                            borderRadius: "3rem",
+                            "&:hover": {
+                                backgroundColor: textHover,
+                            },
+                        }}
+                    >
+                        <DrinkIcon />
+                    </Button>
+                </Box>
             </FlexBetween>
             {/* {error && <p className="error">{error}</p>} */}
             {/* {documents && <ProjectFilter changeFilter={changeFilter} />} */}
             {/* {projects && <ProjectList projects={projects} />} */}
         </WidgetWrapper>
     );
-}
+};
+
+export default FeedSelect;
