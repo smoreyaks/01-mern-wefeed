f9987a8ec5937c373785d3d7abd29ed0ce273c5b
smoreyaks <bnixon.eng@gmail.com>
2024-01-17 19:13:29 +1100

    Added Step Numbers to Panel & Improved Layout Spacing

--- client/src/components/StepsList.jsx
+++ client/src/components/StepsList.jsx
@@ -13,6 +13,8 @@ import {
 // MUI Icons
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 
 // Components
 import FlexBetween from "./FlexBetween";
@@ -20,6 +22,7 @@ import FlexBetween from "./FlexBetween";
 const StepsList = ({ steps, themeColors }) => {
     const [stepsListOpen, setStepsListOpen] = useState(false);
 
+    const stepsCount = Object.keys(steps).length;
     // Theme
     const { palette } = useTheme();
     const {
@@ -49,60 +52,76 @@ const StepsList = ({ steps, themeColors }) => {
                     },
                     width: "100%",
                     // m: "0.5rem 0",
-                    p: "0.5rem  0.75rem",
+                    p: "0.25rem  0.75rem",
                     borderRadius: stepsListOpen
                         ? "0 0 0rem 0rem "
                         : "0 0 0.75rem 0.75rem ",
                     border: "0",
                 }}
             >
-                <Typography variant="h6" fontWeight="bold">
-                    Steps
+                <Typography variant="h6" fontWeight="bold" fontFamily="rubik">
+                    {stepsCount} Steps
                 </Typography>
-                {stepsListOpen ? <RemoveIcon /> : <AddIcon />}
+
+                {stepsListOpen ? (
+                    <KeyboardArrowDownRoundedIcon fontSize="large" />
+                ) : (
+                    <KeyboardArrowRightRoundedIcon fontSize="large" />
+                )}
             </ToggleButton>
 
             {steps.map((step) =>
                 stepsListOpen ? (
-                    <Box
-                        // mr="0.5rem"
-                        sx={{
-                            display: "flex",
-                            gap: "0.5rem",
-                            m: "0",
-                            p: "0.5rem 0.5rem 0 0.5rem",
-                            borderRadius: "0rem",
-                            backgroundColor: recipeStepsPanel,
-                            width: "100%",
-                        }}
-                    >
-                        <Box>
-                            <Typography
-                                key={step._id}
+                    <Box sx={{ py: "0.25rem " }}>
+                        <Box
+                            // mr="0.5rem"
+                            sx={{
+                                display: "flex",
+                                gap: "0.5rem",
+                                m: "0",
+                                p: "0rem 0.5rem",
+                                borderRadius: "0rem",
+                                backgroundColor: recipeStepsPanel,
+                                width: "100%",
+                            }}
+                        >
+                            <Box
                                 sx={{
                                     display: "flex",
-                                    justifyContent: "flex-start",
-                                    alignItems: "center",
-                                    p: "0.25rem 0.5rem 0.25rem 0.5rem",
-                                    fontWeight: "bold",
-                                    background: "#eeceee",
-                                    borderRadius: "0.5rem",
-                                    width: "1.5rem",
+                                    justifyContent: "center",
+                                    alignItems: "flex-start",
                                 }}
                             >
-                                {step.stepNum}
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography
-                                key={step._id}
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "flex-start",
-                                }}
-                            >
-                                {step.stepMethod}
-                            </Typography>
+                                <Typography
+                                    key={step._id}
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        p: "0.25rem 0.25rem 0.25rem 0.25rem",
+                                        fontWeight: "bold",
+                                        fontFamily: "rubik",
+                                        background: "#eeceee",
+                                        borderRadius: "50%",
+                                        width: "1.75rem",
+                                    }}
+                                >
+                                    {step.stepNum}
+                                </Typography>
+                            </Box>
+
+                            <Box sx={{ pt: "0.25rem" }}>
+                                <Typography
+                                    key={step._id}
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "flex-start",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    {step.stepMethod}
+                                </Typography>
+                            </Box>
                         </Box>
                     </Box>
                 ) : (
