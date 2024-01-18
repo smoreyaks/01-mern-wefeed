737d196c57464d34edaba4e2a9c54a28a783c33a
smoreyaks <bnixon.eng@gmail.com>
2024-01-19 03:32:26 +1100

    Welcome Landing Page Update & Visual Polish

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -5,32 +5,56 @@ import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form";
 
 const LoginPage = () => {
-    const theme = useTheme();
+    const { palette } = useTheme();
     const isDesktopScreen = useMediaQuery("(min-width: 1000px)");
 
     return (
         <Box>
             <Box
-                width="100%"
-                backgroundColor={theme.palette.default.background.alt}
-                p="1rem 6%"
-                textAlign="center"
+                sx={{
+                    height: "100vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
             >
-                <Typography fontWeight="bold" fontSize="32px" color="primary">
+                <Typography
+                    variant="h1"
+                    color={palette.default.neutralGrey.white}
+                    fontFamily="PrequelShadow"
+                    fontStyle="bold"
+                    fontSize="4rem"
+                    textAlign="center"
+                    className="childClass"
+                    pb="2rem"
+                    sx={{
+                        transform: "translate(-6rem, 0 rem)",
+                    }}
+                >
+                    Welcome to...
+                </Typography>
+                <Typography
+                    variant="h1"
+                    color={palette.default.neutralGrey.white}
+                    fontFamily="PrequelShadow"
+                    fontStyle="bold"
+                    fontSize="7rem"
+                    textAlign="center"
+                >
                     WeFeed
                 </Typography>
-            </Box>
-            <Box
-                width={isDesktopScreen ? "50%" : "93%"}
-                p="2rem"
-                m="2rem auto"
-                borderRadius="1.5rem"
-                backgroundColor={theme.palette.default.background.alt}
-            >
-                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-                    Welcome to WeFeed!
-                </Typography>
-                <Form />
+
+                <Box
+                    width={isDesktopScreen ? "50%" : "93%"}
+                    p="2rem"
+                    m="2rem auto"
+                    borderRadius="1.5rem"
+                    border="0.5rem solid #fff"
+                    backgroundColor={palette.default.background.alt}
+                >
+                    <Form />
+                </Box>
             </Box>
         </Box>
     );
