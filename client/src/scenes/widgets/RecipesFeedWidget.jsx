4d60bed84db3b31ecbc25a3845ccc7922adbc998
smoreyaks <bnixon.eng@gmail.com>
2024-01-23 18:09:42 +1100

    Fixed Recipe API Issue (State), User Condition now has Visual Cue & has been Disabled

--- client/src/scenes/widgets/RecipesFeedWidget.jsx
+++ client/src/scenes/widgets/RecipesFeedWidget.jsx
@@ -1,18 +1,90 @@
-import { useEffect } from "react";
+// React Components
+
+import { useState, useEffect } from "react";
+
+// Redux & Local State
 import { useDispatch, useSelector } from "react-redux";
 import { setAllRecipes, setRecipe, setPosts } from "../../state";
-import { useState } from "react";
 
-// RecipePost
+// MUI Components
+import { Box, Typography } from "@mui/material";
+
+// Custom Components
 import RecipePostWidget from "./RecipePostWidget";
 
-const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
+const RecipesFeedWidget = ({
+    userId,
+    isProfile = false,
+    themeColors,
+    // user,
+}) => {
     const dispatch = useDispatch();
     const token = useSelector((state) => state.token);
     const friends = useSelector((state) => state.user.friends);
 
+    // Theme Destructure
+    const {
+        primary,
+        whiteText,
+        headingText,
+        textHover,
+        textMain,
+        recipeText,
+        followerIconOutline,
+        followerIconBack,
+        followerIconBackHover,
+        buttonLight,
+        buttonLight2,
+        buttonLight3,
+        buttonHover,
+        backgroundPrimary,
+        backgroundMain,
+        recipeTextPanel,
+        mainBackPanel,
+        recipeStepsPanel,
+        panelMain,
+        recipeStepsPanelHover,
+        panelMainHover,
+    } = themeColors || {};
+
+    // User Destructure - Disabled, Maybe Delete userCondition
+    // const {
+    //     _id,
+    //     // user,
+    //     // userId,
+    //     firstName,
+    //     lastName,
+    //     userPicturePath,
+    //     occupation,
+    //     recipeTitle,
+    //     picturePath,
+    //     ingredients,
+    //     equipment,
+    //     prepTime,
+    //     cookTime,
+    //     servings,
+    //     spiceLevel,
+    //     steps,
+    //     notes,
+    //     tags,
+    //     likes,
+    //     recommendations,
+    //     saves,
+    //     shares,
+    //     comments,
+    //     // themeColors,
+    // } = user || {};
+
     const [user, setUser] = useState(null);
+    const [userRecipes, setUserRecipes] = useState(null);
+
     const recipes = useSelector((state) => state.recipes);
+    console.log("STATE - Recipes:", recipes);
+    // Testing State
+    const [userTest, setUserTest] = useState(null);
+    const [userRecipesTest, setUserRecipesTest] = useState(null);
+    const [recipesTest, setRecipesTest] = useState(null);
+    const [allRecipesTest, setAllRecipesTest] = useState(null);
 
     // ORIGINAL UNCHANGED API CALL - getRecipes
     // const getRecipes = async () => {
@@ -26,6 +98,8 @@ const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
     //     const data = await response.json();
     //     dispatch(setAllRecipes({ recipes: data }));
     // };
+
+    // Retrieves All Recipe Data -  ✅ Works Correctly
     const getRecipes = async () => {
         const response = await fetch(
             "https://server-vukx.onrender.com/recipes",
@@ -36,8 +110,9 @@ const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
         );
         const data = await response.json();
         dispatch(setAllRecipes({ recipes: data }));
-        // setAllRecipes(data);
+        // setRecipesTest(data);
     };
+    // console.log("getRecipes", recipesTest);
 
     // ORIGINAL UNCHANGED API CALL - getUser
     // const getUser = async () => {
@@ -52,6 +127,7 @@ const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
     //     setUser(data);
     // };
 
+    // Retrieves User Data for Selected User -  ✅ Works Correctly
     const getUser = async () => {
         const response = await fetch(
             `https://server-vukx.onrender.com/users/${userId}`,
@@ -62,7 +138,9 @@ const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
         );
         const data = await response.json();
         setUser(data);
+        // setUserTest(data);
     };
+    // console.log("getUserResponse", user);
 
     // ORIGINAL UNCHANGED API CALL - getUserRecipes
     // const getUserRecipes = async () => {
@@ -77,6 +155,7 @@ const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
     //     dispatch(setAllRecipes({ recipes: data }));
     // };
 
+    // Retrieves Recipe Data for Selected User -  ✅ Works Correctly
     const getUserRecipes = async () => {
         const response = await fetch(
             `https://server-vukx.onrender.com/recipes/${userId}/recipes`,
@@ -87,85 +166,112 @@ const RecipesFeedWidget = ({ userId, isProfile = false, themeColors }) => {
         );
         const data = await response.json();
         dispatch(setAllRecipes({ recipes: data }));
+        // setUserRecipes(data);
+        // setUserRecipesTest(data);
     };
+    // console.log("getUserRecipes", userRecipes); // ✅
 
     // Renders either All Recipes or select Profile Recipes
     useEffect(() => {
         if (isProfile) {
-            // getRecipes(); // Doesn't function - Original
-            // getUserRecipes(); // Doesn't function - Original
-            // getUserRecipes();
-            getUserRecipes();
-            getUser();
-        } else {
-            getUser();
-            // getRecipes(); // Doesn't function - Original
             // getUser();
-            // getUser(); // Works as intended
+            // getRecipes();
+            getUserRecipes();
+        } else {
+            // getUser();
+            getRecipes();
         }
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    if (!user) return null;
+    // API Testing
+    console.log("getRecipesTest", recipesTest); // ✅
+    console.log("getUserTest", userTest || "No Data Found"); // ✅
+    console.log("getUserRecipesTest", userRecipesTest); // ✅
+    // console.log("RECIPE FEED USER:", user || "No data found");
+
+    // if (!user) return "Error: No User Found";
 
     // const { formattedFriends } = friends || {};
-    console.log("RECIPE FEED USER:", user);
-    // console.log("USER FEED FRIEND:", friends);
 
     return (
         <>
-            {recipes.map(
-                ({
-                    _id,
-                    user,
-                    userId,
-                    firstName,
-                    lastName,
-                    occupation,
-                    userPicturePath,
-                    recipeTitle,
-                    picturePath,
-                    ingredients,
-                    equipment,
-                    prepTime,
-                    cookTime,
-                    servings,
-                    spiceLevel,
-                    steps,
-                    notes,
-                    tags,
-                    likes,
-                    recommendations,
-                    saves,
-                    shares,
-                    comments,
-                }) => (
-                    <RecipePostWidget
-                        key={_id}
-                        user={user}
-                        recipeId={_id}
-                        recipeUserId={userId}
-                        name={`${firstName} ${lastName}`}
-                        userPicturePath={userPicturePath}
-                        occupation={occupation}
-                        recipeTitle={recipeTitle}
-                        picturePath={picturePath}
-                        ingredients={ingredients}
-                        equipment={equipment}
-                        prepTime={prepTime}
-                        cookTime={cookTime}
-                        servings={servings}
-                        spiceLevel={spiceLevel}
-                        steps={steps}
-                        notes={notes}
-                        tags={tags}
-                        likes={likes}
-                        recommendations={recommendations}
-                        saves={saves}
-                        shares={shares}
-                        comments={comments}
-                        themeColors={themeColors}
-                    />
-                )
+            {!user ? (
+                <Box>
+                    {recipes.map(
+                        ({
+                            _id,
+                            user,
+                            userId,
+                            firstName,
+                            lastName,
+                            occupation,
+                            userPicturePath,
+                            recipeTitle,
+                            picturePath,
+                            ingredients,
+                            equipment,
+                            prepTime,
+                            cookTime,
+                            servings,
+                            spiceLevel,
+                            steps,
+                            notes,
+                            tags,
+                            likes,
+                            recommendations,
+                            saves,
+                            shares,
+                            comments,
+                        }) => (
+                            <RecipePostWidget
+                                key={_id}
+                                user={user}
+                                recipeId={_id}
+                                recipeUserId={userId}
+                                name={`${firstName} ${lastName}`}
+                                userPicturePath={userPicturePath}
+                                occupation={occupation}
+                                recipeTitle={recipeTitle}
+                                picturePath={picturePath}
+                                ingredients={ingredients}
+                                equipment={equipment}
+                                prepTime={prepTime}
+                                cookTime={cookTime}
+                                servings={servings}
+                                spiceLevel={spiceLevel}
+                                steps={steps}
+                                notes={notes}
+                                tags={tags}
+                                likes={likes}
+                                recommendations={recommendations}
+                                saves={saves}
+                                shares={shares}
+                                comments={comments}
+                                themeColors={themeColors}
+                            />
+                        )
+                    )}
+                </Box>
+            ) : (
+                <Box
+                    backgroundColor={backgroundMain}
+                    borderRadius="0.75rem 0.75rem 0.75rem 0.75rem"
+                    p="1.5rem"
+                    my="1rem"
+                >
+                    <Typography
+                        fontWeight="bold"
+                        fontSize="1rem"
+                        color="#F00"
+                        textAlign="center"
+                        borderRadius="0.75rem 0.75rem 0.75rem 0.75rem"
+                        backgroundColor={recipeStepsPanel}
+                        p="1rem"
+                        // m="1rem"
+                    >
+                        Error: No User Found
+                    </Typography>
+                </Box>
             )}
         </>
     );
