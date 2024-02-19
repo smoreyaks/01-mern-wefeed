043c038d52ca8acd5ffcbc2bc2819164b1127d46
smoreyaks <bnixon.eng@gmail.com>
2024-02-20 02:50:22 +1100

    Changed Logo from Text to Image

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,5 +1,9 @@
 import { Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import LogoIcon from "../assets/logo/WE_FEED_LOGO_DARK_20240220a.png";
+import { Image } from "@mui/icons-material";
+
+import { Box } from "@mui/material";
 
 const Logo = ({ themeColors, isDesktopScreen, isMobileMenuToggled }) => {
     // Theme Destructure
@@ -8,24 +12,41 @@ const Logo = ({ themeColors, isDesktopScreen, isMobileMenuToggled }) => {
     const navigate = useNavigate();
 
     return (
-        <Typography
-            fontFamily="PrequelShadow"
-            // fontWeight="bold"
-            fontSize="clamp(1rem, 2rem, 2.25rem)"
-            color={headingText}
+        <Box
+            width="8rem"
             onClick={() => {
                 navigate("/home");
                 // navigate(0);
             }}
             sx={{
+                height: "100%",
+                fontSize: "clamp(1rem, 2rem, 2.25rem)",
+                objectFit: "contain",
+                display: "flex",
+                alignItems: "center",
                 "&:hover": {
                     color: textHover,
                     cursor: "pointer",
                 },
             }}
         >
-            WeFeed
-        </Typography>
+            <img height="100%" width="100%" alt="WeFeed" src={LogoIcon} />
+        </Box>
+
+        // <Typography
+        //     fontFamily="PrequelShadow"
+        //     // fontWeight="bold"
+        //     fontSize="clamp(1rem, 2rem, 2.25rem)"
+        //     color={headingText}
+        //     sx={{
+        // "&:hover": {
+        //     color: textHover,
+        //     cursor: "pointer",
+        // },
+        //     }}
+        // >
+        //     WeFeed
+        // </Typography>
     );
 };
 
