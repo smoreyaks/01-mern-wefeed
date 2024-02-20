8ea7c20c2e8996ca8e71aecc4f1db895392bf0ed
smoreyaks <bnixon.eng@gmail.com>
2024-02-20 17:20:23 +1100

    Changed Ad to Suit Content of WeFeed

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -25,24 +25,30 @@ const AdvertWidget = ({ themeColors }) => {
     return (
         <WidgetWrapper>
             <FlexBetween>
-                <Typography color={headingText} variant="h5" fontWeight="500">
+                <Typography
+                    color={followerIconOutline}
+                    variant="h5"
+                    fontWeight="bold"
+                >
                     Sponsored
                 </Typography>
-                <Typography color={headingText}>Create Ad</Typography>
+                {/* <Typography color={followerIconOutline}>Create Ad</Typography> */}
             </FlexBetween>
             <img
                 width="100%"
                 height="auto"
                 alt="advert"
-                src="https://server-vukx.onrender.com/assets/info4.jpeg"
+                src="https://server-vukx.onrender.com/assets/dinnerlyad1.jpeg"
                 style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
             />
 
-            <Typography color={headingText}>MikaCosmetics.com</Typography>
+            <Typography color={followerIconOutline} fontWeight="bold">
+                Dinnerly.com
+            </Typography>
 
-            <Typography color={headingText} m="0.5rem 0">
-                Your pathway to stunning, immaculate beauty & made sure to make
-                your skin is shining like light.
+            <Typography color={followerIconOutline} m="0.5rem 0">
+                Simple and affordable meal kits. Let Dinnerly take care of your
+                meals. Get your kit today!
             </Typography>
         </WidgetWrapper>
     );
