a050866b17455e479be98a7119dfc75b38cfb5f9
smoreyaks <bnixon.eng@gmail.com>
2024-03-03 17:47:15 +1100

    Created Guest Login Auth Controller Function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcrypt"; // Allows Password Encryption
 import jwt from "jsonwebtoken"; // Grants web token for auth
 import User from "../models/User.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 /* Register User */
 export const register = async (req, res) => {
@@ -69,6 +72,32 @@ export const login = async (req, res) => {
         // Password Error Handling
         if (!isMatch)
             return res.status(400).json({ msg: "Invalid credentials. " });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+/* Guest Logging In */
+export const guestLogin = async (req, res) => {
+    try {
+        // const { email, password } = req.body;
+
+        // Mongoose tries to find specified email
+        const user = await User.findOne({ email: GUEST_EMAIL });
+
+        // User Error Handling
+        if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+        // Validates user Email & Password Credentials
+        const isMatch = await bcrypt.compare(GUEST_PW, user.password);
+
+        // Password Error Handling
+        if (!isMatch)
+            return res.status(400).json({ msg: "Invalid credentials." });
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         delete user.password;
