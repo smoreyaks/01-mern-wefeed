51793342299862eb23efbd5485932d62555326c3
smoreyaks <bnixon.eng@gmail.com>
2024-03-03 17:24:51 +1100

    Made Login Page Responsive

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -9,7 +9,11 @@ import PrequelShadowA from "../../components/svg/PrequelShadowA";
 
 const LoginPage = () => {
     const { palette } = useTheme();
+
+    // Media Queries
     const isDesktopScreen = useMediaQuery("(min-width: 1000px)");
+    const isMediumScreen = useMediaQuery("(min-width: 600px)");
+    const isSmallScreen = useMediaQuery("(min-width: 300px)");
 
     return (
         <Box>
@@ -20,6 +24,7 @@ const LoginPage = () => {
                     flexDirection: "column",
                     justifyContent: "center",
                     alignItems: "center",
+                    m: "0 0.75rem",
                 }}
             >
                 <Typography
@@ -27,37 +32,49 @@ const LoginPage = () => {
                     color={palette.default.neutralGrey.white}
                     fontFamily="PrequelShadow"
                     fontStyle="bold"
-                    fontSize="4rem"
-                    textAlign="center"
-                    className="childClass"
-                    pb="2rem"
-                    sx={{
-                        transform: "translate(-6rem, 0 rem)",
-                    }}
-                >
-                    Welcome to...
-                </Typography>
-                <Typography
-                    variant="h1"
-                    color={palette.default.neutralGrey.white}
-                    fontFamily="PrequelShadow"
-                    fontStyle="bold"
                     fontSize="7rem"
                     textAlign="center"
+                    sx={{
+                        fontSize: isDesktopScreen
+                            ? "7rem"
+                            : isMediumScreen
+                            ? "7rem"
+                            : "4.25rem",
+                    }}
                 >
                     WeFeed
-                    <PrequelShadowA />
+                    {/* <PrequelShadowA /> */}
                 </Typography>
 
                 <Box
-                    width={isDesktopScreen ? "50%" : "93%"}
-                    p="2rem"
-                    m="2rem auto"
+                    width={
+                        isDesktopScreen
+                            ? "40rem"
+                            : isMediumScreen
+                            ? "80%"
+                            : "100%"
+                    }
                     borderRadius="1.5rem"
                     border={`0.25rem solid ${palette.default.neutralGrey.border}`}
                     backgroundColor={palette.default.background.alt}
+                    sx={{
+                        p: isDesktopScreen
+                            ? "2rem"
+                            : isMediumScreen
+                            ? "2rem"
+                            : "1rem",
+                        m: isDesktopScreen
+                            ? "2rem auto"
+                            : isMediumScreen
+                            ? "2rem auto"
+                            : "1rem .75rem",
+                    }}
                 >
-                    <Form />
+                    <Form
+                        isDesktopScreen={isDesktopScreen}
+                        isMediumScreen={isMediumScreen}
+                        isSmallScreen={isSmallScreen}
+                    />
                 </Box>
             </Box>
         </Box>
